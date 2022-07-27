2ae5ef3b8be3f0e50e9588c233f61ecac849723e
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-07-27 11:49:29 +0200

    Add method warning message

--- Motamot/Motamot/Controller/SearchViewController.swift
+++ Motamot/Motamot/Controller/SearchViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var wordTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        searchButton.round()
+    }
+
+    @IBAction func searchButtonPressed() {
+    }
+    
+}
+
+//MARK: -Extension
+extension SearchViewController: SearchDelegate {
+    /**
+ This function displays an alert to user.
+ 
+ - parameter message: String with the message to be display in the alert.
+ */
+    func warningMessage(_ message: String) {
+        let alert: UIAlertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
     }
 
 }
