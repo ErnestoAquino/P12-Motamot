ce63a45e701572110b67f189a1a640cdee420ade
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-07-27 12:15:42 +0200

    Creating Dictionaty Response

---
+++ Motamot/Motamot/Structures/DictionaryResponse.swift
@@ -0,0 +1,39 @@
+//
+//  DictionaryResponse.swift
+//  Motamot
+//
+//  Created by Ernesto Elias Aquino Cifuentes on 27/07/2022.
+//
+
+import Foundation
+
+/**
+ * DictionaryResponse:
+ *
+ * Structure to retrieve the response from: https://dictionaryapi.dev
+ */
+struct DictionaryResponse: Decodable {
+    var word: String?
+    var phonetic: String?
+    var phonetics: [Phonetic]?
+    var meanings: [Meaning]?
+    var sourceUrls: [String]?
+}
+
+struct Phonetic: Decodable {
+    var text: String?
+    var audio: String?
+    var sourceUrl: String?
+}
+
+struct Meaning: Decodable {
+    var definitions: [Definition]?
+    var synonyms: [String]?
+    var antonyms: [String]?
+}
+
+struct Definition: Decodable {
+    var definition: String?
+    var synonyms: [String]?
+    var antonyms: [String]?
+}
