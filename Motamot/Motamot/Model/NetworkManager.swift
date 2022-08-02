425e3bab9e79b3513273ad406c0ac247c9f0f5f0
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-02 15:52:09 +0200

    The network manager now has a new method: getAudio, which allows you to retrieve the audio of the pronunciation from a string as a url.

--- Motamot/Motamot/Model/NetworkManager.swift
+++ Motamot/Motamot/Model/NetworkManager.swift
@@ -19,7 +19,9 @@ public final class NetworkManager<T: Decodable> {
      This function retrieves the contents of a URL based on the specified URL request object, and calls a handler upon completion.
      
      - parameter request:           A URL request object that provides the URL, request type, body data or body stream, and so on.
-     - parameter completionHandler: This completion handler takes the following parameters: T? : The data returned by the server like a structure decodable type. Error? :
+     - parameter completionHandler: This completion handler takes the following parameters:
+     T? : The data returned by the server like a structure decodable
+     type. Error? :
       An error object that indicates why the request failed, or nil if the request was successful.
      */
     public func getInformation(request: URLRequest?, completionHandler: @escaping (T?, Error?) -> Void) {
@@ -79,6 +81,29 @@ public final class NetworkManager<T: Decodable> {
                     return
                 }
                 completionHandler(informationObtained, error)
+            }
+        })
+        task?.resumeWithRequest()
+    }
+
+    public func getAudio(_ urlString: String?, completionHandler: @escaping (Data?)-> Void) {
+        guard let urlString = urlString,
+              let url = URL(string: urlString) else {
+            completionHandler(nil)
+            return
+        }
+       
+        task?.cancel()
+        task = session.dataTaskWithURL(url, completion: { data, response, error in
+            DispatchQueue.main.async {
+                guard error == nil,
+                      let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      let data = data else {
+                    completionHandler(nil)
+                    return
+                }
+                completionHandler(data)
             }
         })
         task?.resumeWithRequest()
