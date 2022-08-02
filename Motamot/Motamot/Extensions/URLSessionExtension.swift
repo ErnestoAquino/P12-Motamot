3bc9230c82b1b52c4f8b39fff4299e62d5a46208
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-02 11:27:23 +0200

    The dataTaskWithURL method has been defined.

--- Motamot/Motamot/Extensions/URLSessionExtension.swift
+++ Motamot/Motamot/Extensions/URLSessionExtension.swift
@@ -9,6 +9,10 @@ import Foundation
 
 // URL Session protocol adn URL Session protocol allow to make the classes testable. Allowing the injection of dependencies.
 extension URLSession: URLSessionProtocol {
+    func dataTaskWithURL(_ request: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        dataTask(with: request, completionHandler: completion) as URLSessionDataTaskProtocol
+    }
+    
     func dataTaskWithRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
         dataTask(with: request, completionHandler: completion) as URLSessionDataTaskProtocol
     }
@@ -21,6 +25,7 @@ extension URLSessionDataTask: URLSessionDataTaskProtocol {
 
 protocol URLSessionProtocol {
     func dataTaskWithRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+    func dataTaskWithURL(_ request: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
 }
 
 protocol URLSessionDataTaskProtocol {
