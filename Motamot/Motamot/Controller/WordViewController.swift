9d57db6d378c94eaf2e00012e483c72a812991bd
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-08 23:01:36 +0200

    Creation of the method that allows to configure the view

--- Motamot/Motamot/Controller/WordViewController.swift
+++ Motamot/Motamot/Controller/WordViewController.swift
@@ -18,9 +18,35 @@ class WordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        pronunciationTextLabel.isEnabled = true
+        playPronunciationButton.isEnabled = true
+        pronunciationTextLabel.isHidden = false
+        playPronunciationButton.isHidden = false
     }
 
     @IBAction func playPronunciationPressed(_ sender: UIButton) {
+        print("Donde estoy?")
+    }
+
+    private func configureView() {
+        wordTextLabel.text = localWord?.word.uppercased()
+//        if localWord?.audio == nil {
+//            pronunciationTextLabel.isHidden = true
+//            playPronunciationButton.isHidden = true
+//        }
+        let formattedText =
+        """
+        Origin: \n\t• \(localWord?.origin ?? "---")\n
+        Phonetics: \n\t• \(localWord?.phonetic ?? "---")\n
+        Definition: \n\t• \(localWord?.definition ?? "---")\n
+        Examples: \n\t• \(localWord?.examples ?? "---")\n
+        Synonyms: \n\t• \(localWord?.synonyms ?? "---")\n
+        Antonyms: \n\t• \(localWord?.antonyms ?? "---")\n
+        Audio: \n\t• \(localWord?.audio?.description ?? "---")\n
+        urlAudio:  \n\t• \(localWord?.urlAudio ?? "---")
+        """
+        definitionTextView.text = formattedText
     }
 
 }
