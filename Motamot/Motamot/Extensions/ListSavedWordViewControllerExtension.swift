ca77aa40f7af7894b4c9fded9a469fb9571fd314
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-10 23:04:10 +0200

    New compliance for table view data source

--- Motamot/Motamot/Extensions/ListSavedWordViewControllerExtension.swift
+++ Motamot/Motamot/Extensions/ListSavedWordViewControllerExtension.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+extension ListSavedWordsViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numberOfRows = localDictionaryService.favoriteWords.count
+        return numberOfRows
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListSavedWordsViewController.cellIdentifier, for: indexPath) as? WordCellTableView else {
+            return UITableViewCell()
+        }
+        let word = localDictionaryService.favoriteWords[indexPath.row].word
+        cell.configureWord(word)
+        return cell
+    }
+}
