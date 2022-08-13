453c1b8eca4422431e5beefc181fae54118f8daa
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-13 12:11:37 +0200

    The tests cover 100 of the networkManager class.

--- Motamot/MotamotTests/NetworkManagerTestCase.swift
+++ Motamot/MotamotTests/NetworkManagerTestCase.swift
@@ -6,30 +6,182 @@
 //
 
 import XCTest
+@testable import Motamot
 
+/**
+ * NetworkManagerTestCase:
+ *
+ * Tests created for the NetworkManager class.
+ */
 class NetworkManagerTestCase: XCTestCase {
+    private var expectation: XCTestExpectation!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        expectation = XCTestExpectation(description: "Waiting for finish.")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    private func createUrlForTest() -> URL? {
+        guard let urlTest = URL(string: "https://urlForTests.com/") else {
+            return nil
         }
+        return urlTest
+    }
+
+//MARK: -Tests
+
+    func testGivenErrorInResponse_WhenCallingGetInformation_ThenShouldHaveAnError() {
+        //Given
+        let session = URLSessionFake(data: nil, response: nil, error: FakeResponse.anError)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        let url = createUrlForTest()
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            //Then
+            XCTAssertNotNil(error)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenUrlNil_WhenCallGetInformation_ThenCompletionHandlerShouldBeNil() {
+        //Given
+        let url: URL? = nil
+        let session = URLSessionFake(data: FakeResponse.correctData, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            //Then
+            XCTAssertNil(error)
+            XCTAssertNil(dictionaryResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenNoDataInResponse_WhenCallGetInformation_ThenCompletionHandlerShoultBeNil() {
+        let url = createUrlForTest()
+        //Given
+        let data: Data? = nil
+        let session = URLSessionFake(data: data, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            //Then
+            XCTAssertNil(error)
+            XCTAssertNil(dictionaryResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenWrongStatusCode_WhenCallGetInformation_ThenCompletionHandlerShouldBeNil() {
+        let url = createUrlForTest()
+        //Given
+        let session = URLSessionFake(data: FakeResponse.correctData, response: FakeResponse.responseFail, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            //Then
+            XCTAssertNil(error)
+            XCTAssertNil(dictionaryResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenIcorrectDataReceived_WhenCallGetInformation_ThenCompletionHandlerShouldBeNil() {
+        let url = createUrlForTest()
+        //Given
+        let session = URLSessionFake(data: FakeResponse.incorrectData, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            XCTAssertNil(error)
+            XCTAssertNil(dictionaryResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenCorrectResponse_WhenCallGetInformation_ThenShouldHaveCorrectResponse() {
+        let url = createUrlForTest()
+        //Given
+        let session = URLSessionFake(data: FakeResponse.correctData, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getInformation(url: url) { dictionaryResponse, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(dictionaryResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenStringUrlNil_WhenCallGetAudio_ThenDataAudioResponseShouldBeNil() {
+        //Given
+        let stringUrl: String? = nil
+        let session = URLSessionFake(data: FakeResponse.audioData, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getAudio(stringUrl) { audioResponse in
+            //Then
+            XCTAssertNil(audioResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenAnError_WhenCallGetAudio_ThenAudioDataResponseShouldBeNil() {
+        //Given
+        let session = URLSessionFake(data: FakeResponse.audioData, response: FakeResponse.responseOK, error: FakeResponse.anError)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getAudio(FakeResponse.url) { audioResponse in
+            //Then
+            XCTAssertNil(audioResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenStatusCodeErrorInResponse_WhenCallGetAudio_ThenAudioDataResponseShouldBeNil() {
+        //Given
+        let session = URLSessionFake(data: FakeResponse.audioData, response: FakeResponse.responseFail, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getAudio(FakeResponse.url) { audioResponse in
+            //Then
+            XCTAssertNil(audioResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenNoDataInResponse_WhenCallGetAudio_ThenAudioDataResponseShouldBeNil() {
+        //Given
+        let session = URLSessionFake(data: nil, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getAudio(FakeResponse.url) { audioResponse in
+            //Then
+            XCTAssertNil(audioResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testGivenCorrectResponse_WhenCallGetAudio_ThenDataAudioResponseShouldBeNoNil() {
+        //Given
+        let session = URLSessionFake(data: FakeResponse.audioData, response: FakeResponse.responseOK, error: nil)
+        let networkManager = NetworkManager<DictionaryResponse>(networkManagerSession: session)
+        //When
+        networkManager.getAudio(FakeResponse.url) { audioResponse in
+            //Then
+            XCTAssertNotNil(audioResponse)
+            self.expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
     }
 
 }
