6f1cfff6359574d32e498eae2743ef56470e6782
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-13 12:08:04 +0200

    The description of the getAudio method has been written.

--- Motamot/Motamot/Model/NetworkManager.swift
+++ Motamot/Motamot/Model/NetworkManager.swift
@@ -17,45 +17,14 @@ public final class NetworkManager<T: Decodable> {
     }
 
     /**
-     This function retrieves the contents of a URL based on the specified URL request object, and calls a handler upon completion.
+     This method retrieves the contents of a URL based on the specified URL request object, and calls a handler upon completion.
      
-     - parameter request:           A URL request object that provides the URL, request type, body data or body stream, and so on.
+     - parameter request:           A URL request object that provides the URL.
      - parameter completionHandler: This completion handler takes the following parameters:
      T? : The data returned by the server like a structure decodable
      type. Error? :
       An error object that indicates why the request failed, or nil if the request was successful.
      */
-    public func getInformation(request: URLRequest?, completionHandler: @escaping (T?, Error?) -> Void) {
-        guard let request = request else {
-            completionHandler(nil, nil)
-            return
-        }
-        task?.cancel()
-        task = session.dataTaskWithRequest(request, completion: { data, response, error in
-            DispatchQueue.main.async {
-                guard error == nil else {
-                    completionHandler(nil, error)
-                    return
-                }
-                guard let data = data,
-                      let response = response as? HTTPURLResponse,
-                      response.statusCode == 200 else {
-                    completionHandler(nil, nil)
-                    return
-                }
-                let decoderData = JSONDecoder()
-                decoderData.keyDecodingStrategy = .useDefaultKeys
-                decoderData.dateDecodingStrategy = .secondsSince1970
-                guard let informationObtained = try? decoderData.decode(T?.self, from: data) else {
-                    completionHandler(nil, nil)
-                    return
-                }
-                completionHandler(informationObtained, error)
-            }
-        })
-        task?.resumeWithRequest()
-    }
-
     public func getInformation(url: URL?, completionHandler: @escaping ([T]?, Error?) -> Void) {
         guard let url = url else {
             completionHandler(nil, nil)
@@ -87,13 +56,18 @@ public final class NetworkManager<T: Decodable> {
         task?.resumeWithRequest()
     }
 
+    /**
+     This method retrives an audio from the url passed in parameter.
+     
+     - parameter urlString:         String for request.
+     - parameter completionHandler: Data? : The data returned by the server if the operation was successful or nil if there was an error
+     */
     public func getAudio(_ urlString: String?, completionHandler: @escaping (Data?)-> Void) {
         guard let urlString = urlString,
               let url = URL(string: urlString) else {
             completionHandler(nil)
             return
         }
-
         task?.cancel()
         task = session.dataTaskWithURL(url, completion: { data, response, error in
             DispatchQueue.main.async {
