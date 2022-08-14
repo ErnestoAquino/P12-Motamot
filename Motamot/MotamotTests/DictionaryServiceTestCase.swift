38121bafdf50931a6546043019d9b6968881e848
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-15 00:22:59 +0200

    New response dictionary with missing elements for tests

--- Motamot/MotamotTests/DictionaryServiceTestCase.swift
+++ Motamot/MotamotTests/DictionaryServiceTestCase.swift
@@ -24,7 +24,58 @@ class DictionaryServiceTestCase: XCTestCase {
     }
 
     func testGivenAnStringWithOnlyBlankSpaces_WhenCallGetDefinition_ThenWarningMessageShouldBeCalled() {
-        
+        let delegate = DictionaryServiceMockDelegate()
+        let dictionaryService = DictionaryService()
+        dictionaryService.viewDelegate = delegate
+
+        //Given
+        let word = "                "
+        //When
+        dictionaryService.getDefinition(word: word)
+        //Then
+        XCTAssertTrue(delegate.warningMessageIsCalled)
+    }
+
+    func testGivenErrorInApiResponse_WhenCallGetDefinition_ThenWarningMessageShouldBeCalled() async {
+        let exp = expectation(description: "Wait for function")
+        //Given
+        let session = URLSessionFake(data: FakeResponse.correctData, response: FakeResponse.responseOK, error: FakeResponse.anError)
+        let delegate = DictionaryServiceMockDelegate()
+        let dictionaryService = DictionaryService(session)
+        dictionaryService.viewDelegate = delegate
+        //When
+        dictionaryService.getDefinition(word: "test")
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        //Then
+        XCTAssertTrue(delegate.warningMessageIsCalled)
+    }
+
+    func testGivenValidRequestAndWord_WhenCallGetDefinition_ThenLocalWordShouldNotBeNil() async {
+        let exp = expectation(description: "Wait for function")
+        //Given
+        let session = URLSessionFake(data: FakeResponse.correctData, response: FakeResponse.responseOK, error: nil)
+        let dictionaryService = DictionaryService(session)
+        //When
+        dictionaryService.getDefinition(word: "test")
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        //Then
+        XCTAssertNotNil(dictionaryService.myLocalWord)
+    }
+
+    func testGivenResponseWithMissing_WhenCallGetDefinition_ThenLocalWordShouldNotBeNil() async {
+        let exp = expectation(description: "Wait for function")
+        //Given
+        let session = URLSessionFake(data: FakeResponse.dataWithMissingElements, response: FakeResponse.responseOK, error: nil)
+        let dictionaryService = DictionaryService(session)
+        //When
+        dictionaryService.getDefinition(word: "test")
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        //Then
+        XCTAssertNotNil(dictionaryService.myLocalWord)
     }
 
 }
+
