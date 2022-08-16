3e49b34d990c1e75cc52416c5000e6a752f467d7
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-16 22:56:12 +0200

    Deleting unnecessary elements

--- Motamot/Motamot/View/WordTableViewCell.swift
+++ Motamot/Motamot/View/WordTableViewCell.swift
@@ -8,33 +8,17 @@
 import UIKit
 
 class WordTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var definitionTextView: UITextView!
-    @IBOutlet weak var pronuciationLabel: UILabel!
-    @IBOutlet weak var playButton: UIButton!
-
-    private var pronunciationSound: Data?
-
+    @IBOutlet weak var wordUILabel: UILabel!
+ 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
-    @IBAction func playButtonPressed(_ sender: Any) {
-        //TODO: La aplicacion va a mostrar los datos del audio."
-        print(pronunciationSound ?? "No data")
-    }
-
-    func configuration (_ word: LocalWord?) {
-        definitionTextView.text = word?.definition
-        if word?.audio != nil {
-            pronunciationSound = word?.audio
-        }
+    func configuration (_ word: String?) {
+        wordUILabel.text = word
     }
 }
