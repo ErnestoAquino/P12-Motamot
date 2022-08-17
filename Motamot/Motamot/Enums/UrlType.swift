418f368032c73ba8d6c12d1eb4f246c53f7412d1
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-08-17 11:26:51 +0200

    adding class decription

--- Motamot/Motamot/Enums/UrlType.swift
+++ Motamot/Motamot/Enums/UrlType.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+/**
+ Types of urls for the createURL method
+ 
+ Available cases:
+ - **urlForDefinition**: Create a url from a word to make a request.
+ */
 enum UrlType {
     case urlForDefinition
 }
